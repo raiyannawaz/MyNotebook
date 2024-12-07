13665ff1e12cb330aab2e373ccb9f4572cb1432c
raiyannawaz <raiyannawaz95@gmail.com>
2024-12-08 00:08:52 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import ContextState from './ContextAPI/ContextState'
 import Navs from './Components/Navs'
 import Alerts from './Components/Alerts'
