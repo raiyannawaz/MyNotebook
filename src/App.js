3e341a0c96f9e04b6d2917424598dfb7fdf0b772
Raiyan-Nawaz <raiyannawaz95@gmail.com>
2024-12-07 20:15:36 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Navs />
           <Alerts />
           <Routes>
-            <Route path='/' element={<Home></Home>} />
+            <Route path='/' element={<Home/>} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
           </Routes>
